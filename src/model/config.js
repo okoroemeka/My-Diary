4dd0cd9225d0cbab16383ef42f8075807cdb58e9
okoroemeka <solomonokoror056@gmail.com>
2018-08-11 14:44:06 -0700

    correct datbase string

--- src/model/config.js
+++ src/model/config.js
@@ -15,10 +15,14 @@ const config = {
   max: 10,
   idleTimeoutMillis: 3000,
 };
-// if (process.env.NODE_ENV === 'test') {
-//   connectionString = 'DiaryTestDb'
-// } if () {
-  
-// }
-const dbConnection = new Pg.Pool(config);
+if (process.env.NODE_ENV === 'test') {
+  connectionString = 'DiaryTestDb'
+} if (process.env.NODE_ENV === 'production') {
+  connectionString = process.env.DATABASE_URL;
+  ssl = true;
+}else{
+  connectionString = config;
+  ssl = false;
+}
+const dbConnection = new Pg.Pool({connectionString,ssl});
 export default dbConnection;
