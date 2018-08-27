bb36acc636003621cf13a120b528f16c4f28b3c5
okoroemeka <solomonokoro056@gmail.com>
2018-08-27 15:44:34 +0100

    remove default event

--- UI/js/script.js
+++ UI/js/script.js
@@ -3,14 +3,14 @@ const lastname = document.getElementById('sign-up-lastname').value;
 const email = document.getElementById('sign-up-email').value;
 const password = document.getElementById('sign-up-password').value;
 const confirmPassword = document.getElementById('sign-up-Confirmpassword').value;
-const createNewUser = (e) => {
+const createNewUser = () => {
     fetch('https://my-dairy-01.herokuapp.com/api/v1/auth/signin',{
     method: 'POST',
     headers: {
         'Accept': 'application/json, text/plain, */*',
         'Content-type': 'application/json',
     },
-    body:JSON.stringify({firstname, lastname,email,password,confirmPassword})
+    body:JSON.stringify({firstname, lastname, email, password, confirmPassword})
 })
 .then(res => res.json())
 .then(user=> console.log(user))
